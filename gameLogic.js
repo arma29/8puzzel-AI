fa22f0c439d0afa7ccb74eefb3a82619c8a9b10e
arma29 <arma@cin.ufpe.br>
2018-08-01 05:23:31 -0300

    Adding Neighbors Function

---
+++ gameLogic.js
@@ -0,0 +1,186 @@
+class GameLogic{
+    constructor(){
+        this.grid = [[], [], []];
+        this.i0 = 0;
+        this.j0 = 0;
+        this.moves = 0;
+
+        this.hfunc = 0;
+        this.priority = 0;
+
+        this.neighbors = undefined;
+        this.previous = null;
+
+    }
+
+    initializeGrid(){
+        var elements = [0,1,2,3,4,5,6,7,8];
+
+        for(let i = 0; i< 3; i++){
+            for(let j = 0; j<3; j++){
+                var chosenIndex = floor(random(elements.length));
+                this.grid[i][j] = elements[chosenIndex];
+                elements.splice(chosenIndex,1);
+                if(this.grid[i][j] == 0){
+                    this.i0 = i;
+                    this.j0 = j;
+                }
+            }
+        }
+    }
+
+    step(x, y) {
+        console.log("i0 e j0 valem:" + this.i0 + ", " + this.j0)
+       if (x > 0 && this.grid[x - 1][y] == 0) {
+          this.swap(x - 1, y, x, y);
+       }
+       else if (y < 2 && this.grid[x][y + 1] == 0) {
+         this.swap(x, y + 1, x, y);
+       }
+       else if (x < 2 && this.grid[x + 1][y] == 0) {
+         this.swap(x + 1, y, x, y);
+       }
+       else if (y > 0 && this.grid[x][y - 1] == 0) {
+         this.swap(x, y - 1, x, y);
+       }
+    }
+
+    step2(xx) {
+        var i0 = this.i0;
+        var j0 = this.j0;
+        //console.log("i0 e j0 valem: " + i0 + " , " + j0);
+        switch(xx){
+            case 2: //UP
+                if(i0 != 2){
+                    this.grid[i0][j0] = this.grid[i0+1][j0];
+                    this.grid[i0+1][j0] = 0;
+                    this.i0++;
+                    this.moves++;
+                }
+                break;
+            case 3: //RIGHT
+                if(j0 != 0){
+                    this.grid[i0][j0] = this.grid[i0][j0-1];
+                    this.grid[i0][j0-1] = 0;
+                    this.j0--;
+                    this.moves++;
+                }
+
+                break;
+
+            case 0: //DOWN
+                if(i0 != 0){
+                    this.grid[i0][j0] = this.grid[i0-1][j0];
+                    this.grid[i0-1][j0] = 0;
+                    this.i0--;
+                    this.moves++;
+                }
+
+                break;
+
+            case 1: //LEFT
+                if(j0 != 2){
+                    this.grid[i0][j0] = this.grid[i0][j0+1];
+                    this.grid[i0][j0+1] = 0;
+                    this.j0++;
+                    this.moves++;
+                }
+                break;
+        }
+    }
+
+    swap(x1, y1, x2, y2) {
+       this.grid[x1][y1] = this.grid[x2][y2];
+       this.grid[x2][y2] = 0;
+    }
+
+    getNeighbors(){
+        if(!this.neighbors){
+            populateNeighbors();
+        }
+        return this.neighbors;
+    }
+
+    getCord(){
+        for(let i = 0; i< 3; i++){
+            for(let j = 0; j<3; j++){
+                if(this.grid[i][j] == 0){
+                    this.i0 = i;
+                    this.j0 = j;
+                }
+            }
+        }
+    }
+
+    copyNode(node){
+        for(let i = 0; i< 3; i++){
+            for(let j = 0; j<3; j++){
+                node.grid[i][j] = this.grid[i][j];
+                if(this.grid[i][j] == 0){
+                    node.i0 = i;
+                    node.j0 = j;
+                }
+            }
+        }
+        node.moves = this.moves;
+
+        node.hfunc = this.hfun;
+        node.priority = this.priority;
+
+        node.neighbors = this.neighbors;
+        node.previous = this.previous;
+
+
+    }
+
+    populateNeighbors(){
+        this.neighbors = [];
+
+
+        //moves;
+        for(var i = 0; i < 4; i++){
+            var node = new GameLogic();
+            //node.grid = this.grid;
+            this.copyNode(node);
+            node.previous = new GameLogic();
+            this.copyNode(node.previous)
+            //node.i0 = this.i0;
+            //node.j0 = this.j0
+            //node.getCord();
+            //console.log("i0 e j0 valem: " + node.i0 + " , " + node.j0);
+            var temp = node.moves;
+            //console.log("temp vale " + temp);
+            //console.table(node.grid);
+
+            node.step2(i);
+            //node.getCord();
+            //console.log("moves vale " + node.moves);
+
+            if(temp != node.moves){
+                //console.table(node.grid);
+                node.hfunc = 111111111111;
+                node.priority = 22222222222222;
+
+                this.neighbors.push(node);
+                //console.table(node.previous.grid);
+            }
+
+
+
+        }
+    }
+
+    getNeighbors(){
+        for(var i = 0; i < this.neighbors.length; i++){
+            console.log(this.neighbors[i].grid);
+        }
+    }
+
+    getFather(){
+        for(var i = 0; i < this.neighbors.length; i++){
+            console.log(this.neighbors[i].previous.grid);
+        }
+    }
+
+
+}
--- sketch.js
+++ sketch.js
@@ -1,7 +1,76 @@
+// type variables
+var type;
+
+// view variables
+var tiles;
+var flex;
+
+// etc
+var moves;
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+  createCanvas(500, 500);
+  type = new GameLogic();
+  type.initializeGrid();
+  type.populateNeighbors();
+  type.getNeighbors();
+  type.getFather();
+  tiles = [[],[],[]];
+  flex = width/3;
+  for(var i = 0; i < 3; i++) {
+    for(var j = 0; j < 3; j++) {
+      tiles[i][j] = new Board(j * flex, i * flex,
+           flex - 3, type.grid[i][j]);
+    }
+  }
 }
 
 function draw() {
+  background(51);
+  drawTiles();
+}
+
+function drawTiles() {
+  for(var i = 0; i < 3; i++) {
+    for(var j = 0; j < 3; j++) {
+      tiles[i][j].update(type.grid[i][j]);
+      tiles[i][j].display();
+    }
+  }
+}
+
+/*function mousePressed() {
+  // print(floor(mouseX / flex), floor(mouseY / flex));
+
+  var clickedX = floor(mouseY / flex);
+  var clickedY = floor(mouseX / flex);
+  console.log(clickedX);
+  console.log(clickedY);
+  // print(clickedX, clickedY);
+  // print(type.grid[clickedX][clickedY]);
+  if(clickedX < 3 && clickedY < 3) {
+    if(type.grid[clickedX][clickedY] != 0) {
+      type.step(clickedX, clickedY);
+    }
+
+    console.log(moves);
+  }
+
+}*/
+
+function keyPressed(){
+
+    if(keyCode == LEFT_ARROW){
+        type.step2(3);
+    }
+    else if(keyCode == UP_ARROW){
+        type.step2(0);
+    }
+    else if(keyCode == DOWN_ARROW){
+        type.step2(2);
+    }
+    else if(keyCode == RIGHT_ARROW){
+        type.step2(1);
+    }
 
 }
